1a4ac4747894b95a53999f6854bd3b73848ea851
mertkarahan955 <you@example.com>
2022-09-01 13:21:31 +0300

    yolla

--- src/kdvHesaplamasi.java
+++ src/kdvHesaplamasi.java
@@ -9,7 +9,7 @@ public class kdvHesaplamasi {
         double urun1;
         Scanner inp = new  Scanner(System.in);
         System.out.print("kdv hesaplamak için urun1 fiyatını giriniz: ");
-        urun1 = inp.nextInt();
+        urun1 = inp.nextDouble();
 
         double  islem = (urun1<= 1000 ) ?    urun1*118/100  : urun1*108/100 ;
         double tutar = (urun1 <=1000) ?     urun1*18/100  : urun1*8/100 ;
