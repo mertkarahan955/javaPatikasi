56cb8c1d6ea99645ffab5c775248c249ef7cb005
mertkarahan955 <you@example.com>
2022-09-01 13:00:22 +0300

    yolla

--- src/ortalamaYapmaProgramı.java
+++ src/ortalamaYapmaProgramı.java
@@ -1,10 +1,14 @@
+
+
 import java.util.Scanner;
 
 public class ortalamaYapmaProgramı {
 
+
+
     public static void main(String[] args) {
-        int mat,fizik,kimya,turkce,tarih,muzik;
-        Scanner inp =  new Scanner(System.in);
+        int mat, fizik, kimya, turkce, tarih, muzik;
+        Scanner inp = new Scanner(System.in);
 
         System.out.print("matematik notunuz: ");
         mat = inp.nextInt();
@@ -24,21 +28,13 @@ public class ortalamaYapmaProgramı {
         System.out.print("müzik notunuzu giriniz : ");
         muzik = inp.nextInt();
 
-       int  toplam  = (mat + fizik + kimya + turkce + tarih + muzik );
-       double sonuc = toplam/6;
-       int gecmeNotu = 50;
-         System.out.print("ortalamanız: " +  sonuc);
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam / 6;
+        int gecmeNotu = 50;
+        System.out.println("ortalamanız: " + sonuc);
 
 
-           
-
-
-
-
-
-
-
-
-
+         String str = (sonuc >= gecmeNotu) ?  "Geçtiniz " : "Kaldınız";
+        System.out.print(str);
        }
 }
